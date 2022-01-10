b6d8f6088a8544586f61047e23520b48b99cebc8
이병훈 <bhlee@onns.co.kr>
2022-01-10 10:08:10 +0900

    tutorial 2 1. login 함수에 listener 연동

--- ModernRIBsTutorial/LoggedOut/LoggedOutInteractor.swift
+++ ModernRIBsTutorial/LoggedOut/LoggedOutInteractor.swift
@@ -19,6 +19,7 @@ protocol LoggedOutPresentable: Presentable {
 
 protocol LoggedOutListener: AnyObject {
     // TODO: Declare methods the interactor can invoke to communicate with other RIBs.
+    func didLogin(withPlayerName player1Name: String, player2Name: String)
 }
 
 final class LoggedOutInteractor: PresentableInteractor<LoggedOutPresentable>, LoggedOutInteractable, LoggedOutPresentableListener {
@@ -47,7 +48,7 @@ final class LoggedOutInteractor: PresentableInteractor<LoggedOutPresentable>, Lo
         
         let player1NameWithDefault = playerName(player1Name, withDefaultName: "Player 1")
         let player2NameWithDefault = playerName(player2Name, withDefaultName: "Player 2")
-        
+        listener?.didLogin(withPlayerName: player1NameWithDefault, player2Name: player2NameWithDefault)
             print("\(player1NameWithDefault) vs \(player2NameWithDefault)")
     }
     
