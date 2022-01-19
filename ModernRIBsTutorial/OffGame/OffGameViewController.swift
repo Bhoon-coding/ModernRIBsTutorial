68f23c3397bda1046790f4ea30154467a04c6696
이병훈 <bhlee@onns.co.kr>
2022-01-19 17:53:17 +0900

    tutorial 3 Passing the player names to the OffGame scope using the DI tree

--- ModernRIBsTutorial/OffGame/OffGameViewController.swift
+++ ModernRIBsTutorial/OffGame/OffGameViewController.swift
@@ -25,7 +25,9 @@ final class OffGameViewController: UIViewController, OffGamePresentable, OffGame
 
     weak var listener: OffGamePresentableListener?
     
-    init() {
+    init(player1Name: String, player2Name: String) {
+        self.player1Name = player1Name
+        self.player2Name = player2Name
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -38,6 +40,7 @@ final class OffGameViewController: UIViewController, OffGamePresentable, OffGame
         
         view.backgroundColor = UIColor.yellow
         buildStartButton()
+        buildPlayerLabels()
     }
     
     // MARK: - Private
@@ -58,4 +61,45 @@ final class OffGameViewController: UIViewController, OffGamePresentable, OffGame
     @objc private func didTapStartButton() {
         listener?.startGame()
     }
+    
+    private func buildPlayerLabels() {
+           let labelBuilder: (UIColor, String) -> UILabel = { (color: UIColor, text: String) in
+               let label = UILabel()
+               label.font = UIFont.boldSystemFont(ofSize: 35)
+               label.backgroundColor = UIColor.clear
+               label.textColor = color
+               label.textAlignment = .center
+               label.text = text
+               return label
+           }
+        let player1Label = labelBuilder(UIColor.blue, player1Name)
+        view.addSubview(player1Label)
+        player1Label.snp.makeConstraints { (maker: ConstraintMaker) in
+            maker.top.equalTo(self.view).offset(70)
+            maker.leading.trailing.equalTo(self.view).inset(20)
+            maker.height.equalTo(40)
+        }
+        
+        let vsLabel = UILabel()
+        vsLabel.font = UIFont.systemFont(ofSize: 25)
+        vsLabel.backgroundColor = UIColor.clear
+        vsLabel.textColor = UIColor.darkGray
+        vsLabel.textAlignment = .center
+        vsLabel.text = "vs"
+        view.addSubview(vsLabel)
+        vsLabel.snp.makeConstraints { make in
+            make.top.equalTo(player1Label.snp.bottom).offset(10)
+            make.leading.trailing.equalTo(player1Label)
+            make.height.equalTo(20)
+        }
+        
+        let player2Label = labelBuilder(UIColor.red, player2Name)
+        view.addSubview(player2Label)
+        player2Label.snp.makeConstraints { make in
+            make.top.equalTo(vsLabel.snp.bottom).offset(10)
+            make.height.leading.trailing.equalTo(player1Label)
+        }
+    }
+    private let player1Name: String
+    private let player2Name: String
 }
